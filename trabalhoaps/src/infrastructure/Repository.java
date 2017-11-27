b336108c8f01f70dd9e494218c056a188b0d7b0b
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-11-27 18:24:41 -0200

    Move Singleton para o Repository

--- trabalhoaps/src/infrastructure/Repository.java
+++ trabalhoaps/src/infrastructure/Repository.java
@@ -6,5 +6,14 @@ import java.util.List;
 import model.Reserva;
 
 public class Repository {
+	private static Repository instance;
+	
+	public static Repository getInstance(){
+		if(instance == null){
+			instance = new InfraFacade();
+		}
+		return instance;
+	}
+	
 	public static List<Reserva> reservas = new ArrayList<Reserva>();
 }
