50fea5c8812606f859bc13ed56b45e639e1f65c4
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-11-30 17:43:46 -0200

    Update ViewFacade.java

--- trabalhoaps/src/view/ViewFacade.java
+++ trabalhoaps/src/view/ViewFacade.java
@@ -2,7 +2,7 @@ package view;
 
 
 public class ViewFacade {
-	
+	//Facade - Questão 2
 	public static void invoke(String command){
 		System.out.println("Chama o invoker");
 		Invoker.invoke(command);		
