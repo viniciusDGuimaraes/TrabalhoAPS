4e0adbdb983a94a41481290a28ea101a4beeec46
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-11-30 17:44:18 -0200

    Update ApplicationFacade.java

--- trabalhoaps/src/Application/ApplicationFacade.java
+++ trabalhoaps/src/Application/ApplicationFacade.java
@@ -3,7 +3,8 @@ package Application;
 import model.Livro;
 import model.Usuario;
 
-public class ApplicationFacade {	
+public class ApplicationFacade {
+	//Facade - Questão 2
 	public static void reservarLivro(Livro l, Usuario u){
 		System.out.println("Chama o UsuarioServices");
 		UsuarioServices.reservaLivro(l, u);
