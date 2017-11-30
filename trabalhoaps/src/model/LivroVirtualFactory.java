c6b05350c899b0ecda8c549c5dcb1ae70f272c06
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-11-30 18:34:56 -0200

    Update LivroVirtualFactory.java

--- trabalhoaps/src/model/LivroVirtualFactory.java
+++ trabalhoaps/src/model/LivroVirtualFactory.java
@@ -1,9 +1,11 @@
 package model;
 
+//Abstract factory do livro virtual
 public class LivroVirtualFactory extends LivroFactory{
 
 	@Override
 	public Livro criarLivro() {
+		System.out.println("Cria livro virtual");
 		return new LivroVirtual();
 	}
 
