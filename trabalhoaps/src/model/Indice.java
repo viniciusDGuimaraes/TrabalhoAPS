5e7341c4be5d7d07bbfe1ea58420952a3cc771d4
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-11-30 17:48:02 -0200

    Update Indice.java

--- trabalhoaps/src/model/Indice.java
+++ trabalhoaps/src/model/Indice.java
@@ -3,6 +3,7 @@ package model;
 import java.util.List;
 
 public abstract class Indice {
+	//Composite - Questão 5
 	public abstract void adicionar(Indice s);
 	
 	public abstract void remover(Indice s);
