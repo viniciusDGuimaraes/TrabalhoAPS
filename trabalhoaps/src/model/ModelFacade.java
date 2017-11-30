0c060c83a58ff1515329892976083793ad0ffec1
Vinicius Guimarães <vinicius.dalmeidaguimaraes@gmail.com>
2017-11-30 17:43:57 -0200

    Update ModelFacade.java

--- trabalhoaps/src/model/ModelFacade.java
+++ trabalhoaps/src/model/ModelFacade.java
@@ -1,7 +1,7 @@
 package model;
 
 public class ModelFacade {
-	
+	//Facade - Questão 2
 	public static void reservarLivro(Livro l, Usuario u){
 		System.out.println("Chama método reservar da classe livro");
 		l.reservar(u);
